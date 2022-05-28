6e7e0aa7a016e9e67661bcb68e3a0f9ba31da511
Harmeto <bad.thg@gmail.com>
2022-05-27 21:27:32 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         alt = "logo" / >
         <
         p >
-        Edit < code > src / App.js < /code> and save to reload. < /
+        Pagina Subida por Carlos Salvo < /
         p > <
         a className = "App-link"
         href = "https://reactjs.org"
