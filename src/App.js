82d9aab6d9268964f6747977e6c3b5f87d2043c5
Harmeto <programador.web@samurai.cl>
2022-06-08 20:12:54 -0400

    Dev branch updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         alt = "logo" / >
         <
         p >
-        Pagina Subida por Carlos Salvo < /
+        Rama Dev, subida por Carlos Salvo < /
         p > <
         a className = "App-link"
         href = "https://reactjs.org"
